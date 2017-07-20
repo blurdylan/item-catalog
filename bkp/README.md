b7c00df461235939fbc03ea9032a97728ab26343
Dylan Tientcheu <dylanmambou17@gmail.com>
2017-07-20 21:37:09 +0100

    * Fixed login * New interface

--- bkp/README.md
+++ bkp/README.md
@@ -1,19 +1,12 @@
 # Item Catalog Web App
+> By Dylan Blur
+
 This web app is a project for the Udacity [FSND Course](https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd004).
 
-## About
-This project is a RESTful web application utilizing the Flask framework which accesses a SQL database that populates categories and their items. OAuth2 provides authentication for further CRUD functionality on the application. Currently OAuth2 is implemented for Google Accounts.
 
-## In This Repo
+## In This Reposition
 This project has one main Python module `app.py` which runs the Flask application. A SQL database is created using the `database_setup.py` module and you can populate the database with test data using `database_init.py`.
 The Flask application uses stored HTML templates in the tempaltes folder to build the front-end of the application. CSS/JS/Images are stored in the static directory.
-
-## Skills Honed
-1. Python
-2. HTML
-3. CSS
-4. OAuth
-5. Flask Framework
 
 ## Installation
 There are some dependancies and a few instructions on how to run the application.
@@ -49,16 +42,15 @@ To get the Google login working there are a few additional steps:
 5. Select Web application
 6. Enter name 'Item-Catalog'
 7. Authorized JavaScript origins = 'http://localhost:5000'
-8. Authorized redirect URIs = 'http://localhost:5000/catalog'
+8. Authorized redirect URIs = 'http://localhost:5000/login' && 'http://localhost:5000/gconnect'
 9. Select Create
-10. Copy the Client ID and paste it into the `data-clientid` in login.html, category_detail.html, create_item.html, delete_item.html,edit_item.html,main.html
+10. Copy the Client ID and paste it into the `data-clientid` in login.html
 11. On the Dev Console Select Download JSON
 12. Rename JSON file to client_secrets.json
 13. Place JSON file in item-catalog directory that you cloned from here
-14. Run application using `python project.py`
+14. Run application using `python /item-catalog/app.py`
 
 ## JSON Endpoints
-The following are open to the public:
 
 Catalog JSON: `/catalog/JSON`
     - Displays the whole catalog. Categories and all items.
--- database_setup.py
+++ database_setup.py
@@ -1,60 +1,59 @@
+import os
+import sys
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-
+import datetime
 
 Base = declarative_base()
 
-
-class User(Base):
-    __tablename__ = 'user'
-
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable = False)
-    email = Column(String(250), nullable = False)
-    picture = Column(String(250))
+def get_current_time():
+    return datetime.datetime.now()
 
 class Category(Base):
     __tablename__ = 'category'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    date = Column(DateTime, default=get_current_time,
+        onupdate=get_current_time)
+        # automatically updates on creation and update
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(255), nullable = False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User, backref="category")
+    def __init__(self, name):
+        self.name = name
 
     @property
     def serialize(self):
-        """Return object data in easily serializeable format"""
+        """Return serializeable format of the Category Object"""
         return {
-            'name'          : self.name,
-            'id'            : self.id
+            'name'  : self.name,
+            'id'    : self.id,
         }
 
-class Items(Base):
-    __tablename__ = 'items'
+class CategoryItem(Base):
+    __tablename__ = 'category_item'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    date = Column(DateTime, nullable=False)
-    description = Column(String(250))
-    picture = Column(String(250))
+    title = Column(String(80), nullable=False)
+    description = Column(String(250), nullable=False)
     category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(Category, backref=backref('items', cascade='all, delete'))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User, backref="items")
+    category = relationship(Category)
+    date = Column(DateTime, default=get_current_time,
+        onupdate=get_current_time)
+
+    def __init__(self, title, description, category_id):
+        self.title = title
+        self.description = description
+        self.category_id = category_id
 
     @property
     def serialize(self):
-        """Return object data in easily serializeable format"""
+        """Return serializeable format of the CategoryItem Object"""
         return {
-            'name'          : self.name,
             'id'            : self.id,
+            'title'          : self.title,
             'description'   : self.description,
-            'picture'       : self.picture,
-            'category'      : self.category.name
         }
-
 
 engine = create_engine('sqlite:///catalog.db')
 
